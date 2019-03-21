7cb48e21b33776fba31341ff8588bddb6ef0b11d
DarkSoul-SP <viytenko@gmail.com>
2019-03-21 17:55:36 +0200

    start frame

---
+++ src/main/java/ua/darksoul/testprojects/ownchat/domain/Role.java
@@ -0,0 +1,5 @@
+package ua.darksoul.testprojects.ownchat.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/ua/darksoul/testprojects/ownchat/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package ua.darksoul.testprojects.ownchat.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.darksoul.testprojects.ownchat.domain.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
