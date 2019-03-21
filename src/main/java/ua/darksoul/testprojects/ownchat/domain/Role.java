632449d17e152beb61c28a6418bfd4ffe6dfdc10
DarkSoul-SP <viytenko@gmail.com>
2019-03-21 20:04:24 +0200

    Migration for template engine (mustache -> freemarker) + added coupling for entities Message(manyToOne) and User.

--- src/main/java/ua/darksoul/testprojects/ownchat/domain/Role.java
+++ src/main/java/ua/darksoul/testprojects/ownchat/domain/Role.java
@@ -1,5 +1,12 @@
 package ua.darksoul.testprojects.ownchat.domain;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
